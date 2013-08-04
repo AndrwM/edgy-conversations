358082b80fda3cdf4c93619af73ed22cc42fc355
Andrew McIntee <me@andrewmcintee.com>
2013-08-04 10:44:49 -0400

    Smooth scrolling and other tweaks

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
   
   resizeHeader();
+  scrollToId();
+
+  $( ".play-video" ).click(function() {
+    playHeadVideo ();
+    return false;
+  });
 
 });
 
@@ -9,9 +15,34 @@ function resizeHeader () {
   var header = $('header');
   var windowHeight = $(window).height();
 
-  var headerHeight = ( windowHeight - 88 )
+  var headerHeight = ( windowHeight - 66 )
 
   header.height(headerHeight)
   header.backstretch(["images/header-bg.jpg"], {centeredY: false, fade:1200});
 
 };
+
+function playHeadVideo () {
+  $('.video-promo,.backstretch img').fadeOut(500);
+};
+
+function scrollToId () {
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (
+      location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+      || location.hostname == this.hostname
+    ) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+  });
+
+}
