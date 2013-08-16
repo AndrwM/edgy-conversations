d62714709b07b2b9debfbe1accbc7daf73cd33ae
Andrew McIntee <me@andrewmcintee.com>
2013-08-16 14:32:13 -0400

    Wired up dem forms to le confusionsoft

--- index.php
+++ index.php
@@ -29,10 +29,13 @@
 </header>
 
 <section class="signup">
-  <form class="wrapper">
-    <input class="required" type="text" value="" placeholder="First Name">
-    <input class="required" type="text" value="" placeholder="Email Address">
-    <a href="#" class="submit btn">Sign Me Up</a>
+  <form accept-charset="UTF-8" name="form1" action="https://bd137.infusionsoft.com/app/form/process/6193b3f4eb16d94dfef56ce14ce05340" class="infusion-form wrapper" method="POST">
+    <input name="inf_form_xid" type="hidden" value="6193b3f4eb16d94dfef56ce14ce05340" />
+    <input name="inf_form_name" type="hidden" value="Tell me when I&#a;can get Dan's book" />
+    <input name="infusionsoft_version" type="hidden" value="1.29.4.53" />  
+    <input class="infusion-field-input-container" id="inf_field_FirstName" name="inf_field_FirstName" type="text"value="" placeholder="First Name" />
+    <input class="infusion-field-input-container" id="inf_field_Email" name="inf_field_Email" type="text" value="" placeholder="Email Address" />
+    <a href="javascript: document.form1.submit();" class="submit btn" >Sign Me Up</a>
   </form>
 </section>
 
@@ -67,10 +70,13 @@
     <h1>Pre-order Edgy Conversations</h1>
     <h2>Be the first to get this life changing book!</h2>
 
-    <form>
-      <input class="required" type="text" value="" placeholder="First Name">
-      <input class="required" type="text" value="" placeholder="Email Address">
-      <a href="#" class="submit btn">Sign Me Up</a>
+    <form accept-charset="UTF-8" name="form2" action="https://bd137.infusionsoft.com/app/form/process/6193b3f4eb16d94dfef56ce14ce05340" class="infusion-form" method="POST">
+      <input name="inf_form_xid" type="hidden" value="6193b3f4eb16d94dfef56ce14ce05340" />
+      <input name="inf_form_name" type="hidden" value="Tell me when I&#a;can get Dan's book" />
+      <input name="infusionsoft_version" type="hidden" value="1.29.4.53" />  
+      <input class="infusion-field-input-container" id="inf_field_FirstName" name="inf_field_FirstName" type="text"value="" placeholder="First Name" />
+      <input class="infusion-field-input-container" id="inf_field_Email" name="inf_field_Email" type="text" value="" placeholder="Email Address" />
+      <a href="javascript: document.form2.submit();" class="submit btn" >Sign Me Up</a>
       <span>Book pre-orders begin July 30th</span>
     </form>
 
